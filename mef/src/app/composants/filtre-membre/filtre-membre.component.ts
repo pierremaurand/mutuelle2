5c9553f740e1f339c78065e3549be76b3da99ef7
Ovassa Pierre Maurand <pierremaurand@gmail.com>
2023-10-19 22:49:37 +0100

    new file:   mef/src/app/composants/filtre-membre/filtre-membre.component.html 	new file:   mef/src/app/composants/filtre-membre/filtre-membre.component.scss 	new file:   mef/src/app/composants/filtre-membre/filtre-membre.component.ts

---
+++ mef/src/app/composants/filtre-membre/filtre-membre.component.ts
@@ -0,0 +1,58 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { Observable, combineLatest, map, startWith } from 'rxjs';
+import { Membre } from 'src/app/models/membre.model';
+import { MembreService } from 'src/app/services/membre.service';
+
+@Component({
+  selector: 'app-filtre-membre',
+  templateUrl: './filtre-membre.component.html',
+  styleUrls: ['./filtre-membre.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FiltreMembreComponent implements OnInit {
+  membres$!: Observable<Membre[]>;
+
+  @Output()
+  membresFound = new EventEmitter<Membre[]>();
+
+  searchCtrl!: FormControl;
+
+  constructor(private membreService: MembreService, private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initForm();
+    this.initObservables();
+  }
+
+  private initForm(): void {
+    this.searchCtrl = this.fb.control('');
+  }
+
+  private initObservables(): void {
+    this.membres$ = this.membreService.membres$;
+
+    const search$ = this.searchCtrl.valueChanges.pipe(
+      startWith(this.searchCtrl.value),
+      map((value) => value.toLowerCase())
+    );
+
+    this.membres$ = combineLatest([search$, this.membreService.membres$]).pipe(
+      map(([search, membres]) =>
+        membres.filter((membre) =>
+          membre.nom.toLowerCase().includes(search as string)
+        )
+      )
+    );
+
+    this.membres$.subscribe((membres: Membre[]) => {
+      this.membresFound.emit(membres);
+    });
+  }
+}
